718de40b1b8e545e8137dcd1ed740f839256a4a5
Tushita Wadhawan <twadhawan@berkeley.edu>
2025-02-06 13:42:02 -0800

    Adding homework 2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -11,7 +11,10 @@
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
 # "git add" adds code to the git staging area ie when updating files or changes. "Git commit" commits changes to local repository and saves them with a message. "Git push" uploads code to remote github repository
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
-# 
+# The message that appears is: modified:  homework2/hw2.py. It is telling me that hw2.py has some changes and is modified. 
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# I was running into an error when attempting to copy the "homework2" directory from brianna_repo into tushita_decal using the cp command and it was very annoying because it kept moving different folders into other folders. I resolved it by simply copying and pasting the homework2 folder into mine through File Explorer. 
 # 9) How does cloning a repository differ from pulling from a repository?
+# Cloning a repository means to create a copy of someone else's repo from GitHub to your computer as a local copy. Pulling from a repository means to update the local copy on your computer with new commits from remote repository; it is used a lot when collaborating with others. 
 # 10) Tell me a fun fact!
+# Hippos are widely considered to be one of the most dangerous large animals in Africa because they attack humans and crocodiles with no apparent provocation. 
